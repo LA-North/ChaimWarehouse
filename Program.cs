bfaec3fb5480935cde84906fa5ffae85b9857d45
ChaimEzra <chimas1357@gmail.com>
2026-03-04 17:04:41 +0200

    Added configs and project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ChaimWarehouse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
