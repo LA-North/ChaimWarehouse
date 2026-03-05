279bd9dd58296b61c9475efeaaa4d5e4fa97e468
ChaimEzra <chimas1357@gmail.com>
2026-03-05 16:44:24 +0200

    Fix second pr comments.

---
+++ Commands/ICommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChaimWarehouse.Commands
+{
+   
+    /// <summary>
+    /// Interface for commands that can only run but cannot be undone.
+    /// Used for actions like help, listing items or queries.
+    /// </summary>
+    public interface INotUndoCommand
+    {
+        void Execute(string[] args);
+    }
+}
