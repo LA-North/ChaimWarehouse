fc911ac9b709f68383330001b632fccac1d5a173
ChaimEzra <chimas1357@gmail.com>
2026-03-10 17:50:26 +0200

    Fix decomantation for warehouse class.

--- WarehouseFolder/Warehouse.cs
+++ WarehouseFolder/Warehouse.cs
@@ -10,44 +10,92 @@ namespace ChaimWarehouse.WarehouseFolder
     /// </summary>
     public class Warehouse
     {
+        /// <summary>
+        /// Holds the single instance of the warehouse (Singleton instance).
+        /// </summary>
         private static Warehouse warehouse;
+
+        /// <summary>
+        /// Event raised when an item reaches a low stock condition.
+        /// </summary>
         public event EventHandler<LowStockEventArgs> LowStock;
+
+        /// <summary>
+        /// Collection that stores the items and their current quantities.
+        /// </summary>
         public IDictionary<Item, int> ItemsInWarehouse { get; set; }
 
+        /// <summary>
+        /// Private constructor to prevent external creation of the warehouse instance.
+        /// </summary>
         private Warehouse() { }
-       
+
+        /// <summary>
+        /// Returns the single warehouse instance.
+        /// </summary>
         public static Warehouse GetWarehouse()
         {
-           return null;
+            return null;
         }
-        
+
+        /// <summary>
+        /// Adds a new item to the warehouse.
+        /// </summary>
+        /// <param name="item">The item to add to the warehouse.</param>
         public void AddNewItem(Item item)
         {
-            
+
         }
-        
+
+        /// <summary>
+        /// Removes an item from the warehouse.
+        /// </summary>
+        /// <param name="id">The item id to remove from the warehouse.</param>
         public void RemoveItem(int id) { }
-        
+
+        /// <summary>
+        /// Increases the quantity of an existing item.
+        /// </summary>
+        /// <param name="id">The identifier of the item whose quantity going to be add.</param>
+        /// <param name="quantity">The quantity to add.</param>
         public void AddStock(int id, int quantity)
         {
-            
+
         }
-        
+
+        /// <summary>
+        /// Decreases the quantity of an existing item.
+        /// </summary>
+        /// <param name="id">The identifier of the item whose quantity going to be remove.</param>
+        /// <param name="quantity">The quantity to remove.</param>
         public void RemoveStock(int id, int quantity)
         {
 
         }
-        
+
+        /// <summary>
+        /// Displays the current state of the warehouse.
+        /// </summary>
         public void PrintWarehouse()
         {
-           
-        }
-        
-        public void UpdateQuantity(int id) { }
 
+        }
+
+        /// <summary>
+        /// Called after an item's quantity changes to evaluate warehouse rules.
+        /// </summary>
+        /// <param name="id">The identifier of the item whose quantity was changed.</param>
+        public void UpdatedQuantity(int id) { }
+
+        /// <summary>
+        /// Raises the LowStock event when a low stock condition occurs.
+        /// </summary>
+        /// <param name="item">The item that his quantity is below the threshold.</param>
+        /// <param name="quantity">What quantity trying to be changed.</param>
+        /// <param name="threshold">The threshold that read from the configuration file.</param>
         protected virtual void OnLowStock(Item item, int quantity, int threshold)
         {
-          
+
         }
     }
 }
